4c8c4dfe61bc7f12c6ea34dba592248a2741e17a
Borisresonance <llorentejuandiego@gmail.com>
2024-12-09 15:47:25 -0500

    auth changes

--- auth.ts
+++ auth.ts
@@ -1,36 +1,42 @@
 import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import type { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
 
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
+ 
 export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
   providers: [
     Credentials({
-      name: 'Credentials',
       async authorize(credentials) {
-        // Hardcoded credentials
-        const hardcodedUser = {
-          email: 'admin@example.com',
-          password: '123456', 
-        };
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+ 
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
 
-        // Validate credentials
-        if (
-          credentials?.email === hardcodedUser.email &&
-          credentials?.password === hardcodedUser.password
-        ) {
-          // Return user object on success
-          return { id: '1', name: 'Admin', email: hardcodedUser.email };
+          if (passwordsMatch) return user;
         }
-
         console.log('Invalid credentials');
-        return null; // Return null if authentication fails
+        return null;
+ 
       },
     }),
   ],
-  // Optional: Customize pages for errors or redirects
-  pages: {
-    signIn: '/auth/signin',
-    error: '/auth/error', // Error page
-  },
-  // Optional: Enable debug mode for development
-  debug: process.env.NODE_ENV === 'development',
 });
